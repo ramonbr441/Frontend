1e3b5d320676b9cebe67ee1f5074d27969a9780e
Ramon Alves Carvalho <Ramonbr441@gmail.com>
2024-04-26 20:11:14 -0300

    Hello world

---
+++ script.js
@@ -0,0 +1,118 @@
+// exercicio 1
+
+
+// var nomes = ["ana", "carlos", "bruna", "juca", "zeze", "lulu"];
+// var notas = [8.3, 7.2, 9,  7.5, 8.2, 10];
+
+
+// function maiorNota(nomes, notas){
+
+//     let notao = 0;
+//     let melhorAluno = "nenhum";
+
+
+//     for(let i =0; i< notas.length; i++ ){
+//         if(notao < notas[i]){
+//             notao = notas[i];
+//             melhorAluno = nomes[i];
+//         }
+
+
+//     }
+
+//     console.log(melhorAluno + " tirou a maior nota:" + notao);
+// }
+
+// exerciocio 2
+
+
+// var inteiros = [1,2,3,4,5,6,7,8,9,10];
+
+// function inverteArray(inteiros){
+
+
+//     let invertido = [];
+
+//     for (numero of inteiros){
+//         invertido.unshift(numero);
+//     }
+
+    
+
+//     console.log(invertido);
+
+
+// }
+
+// var menores = [1,2,3,4,5];
+// var maiores = [6,7,8,9,10];
+
+
+// function uneArrays(menores, maiores){
+
+//     let total = new Array(menores.lenght + maiores.lenght);
+
+//     for (let i = 0; i < menores.lenght; i ++ ){
+
+//         total[i] = menores[i];
+//     }   
+
+//     for (let i = 0; i < maiores.lenght; i++){
+
+//         total[menores.lenght + i] = maiores [i];
+//     }
+
+//     console.log(total);
+// }
+
+// uneArrays(menores,maiores);
+
+
+// EXERCICIO 1 
+
+// var numeros = [1,2,3,4,5,6,7]
+
+// function somaElementos(numeros){
+
+//     let total = 0;
+
+//     for(let i = 0; i < numeros.length; i++){
+
+//         total += numeros[i];
+
+//     }
+
+//     console.log(total);
+// }
+
+
+// EXERCICIO 2
+
+// var numeros = [1,2,3,4,5,6,7]
+
+// function mediaElementos(numeros){
+
+//     let total = 0;
+//     let media = 0;
+
+//     for (let i = 0; i< numeros.length; i++){
+//         total += numeros[i];
+         
+//     }
+
+//     media = total/ numeros.length;
+
+//     console.log(media);
+
+
+// }
+
+
+// EXERCICIO 3
+
+var numeros = [1,2,3,4,5,6,7]
+
+function menorNumero(numeros){
+
+    
+}
